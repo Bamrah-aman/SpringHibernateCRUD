fc4c192027c029ef5722d06687be7b5ef3bd8e45
Jaspreet Singh <jaspreet.singh2@bell.ca>
2023-05-17 15:47:21 +0530

    Added Demo Controller

---
+++ src/main/java/com/luv2code/springdemo/controller/DemoController.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo.controller;
+
+public class DemoController {
+
+    public static void main(String[] args) {
+        System.out.println("Demo controller for checkup");
+    }
+
+}
